63fd94ddbbd8e93c92016a7034e21b8d9b9d0ea7
Dongwon Lee <ldw3097@gmail.com>
2024-01-17 20:24:24 +0900

    Title: 2차원 최대 증가 수열, Time: 198ms, Memory: 25MB, Status: Passed - CodeTree

--- 240117/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
+++ 240117/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
@@ -1,11 +1,12 @@
 import sys
 input = sys.stdin.readline
-n, m = map(int, input().split())
+n, m = list(map(int, input().split()))
 arr = []
 for _ in range(n):
     arr.append( list(map(int, input().split()))  )
 
 dp = [[0]*m for _ in range(n)]
+dp[0][0] = 1
 globalmax = 0
 for i in range(n):
     for j in range(m):
@@ -14,6 +15,7 @@ for i in range(n):
             for l in range(j):
                 if arr[i][j] > arr[k][l]:
                     maxval = max(maxval, dp[k][l])
-        dp[i][j] = maxval + 1
-        globalmax = max(globalmax, dp[i][j])
+        if maxval != 0:
+            dp[i][j] = maxval + 1
+            globalmax = max(globalmax, dp[i][j])
 print(globalmax)
