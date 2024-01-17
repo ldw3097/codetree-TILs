4d86a606e5d3d655a8b98cb804e5edc16f7281a7
Dongwon Lee <ldw3097@gmail.com>
2024-01-17 17:51:46 +0900

    Title: 최대 점프 횟수, Time: 149ms, Memory: 25MB, Status: Passed - CodeTree

--- 240117/최대 점프 횟수/maximum-number-of-jumps.py
+++ 240117/최대 점프 횟수/maximum-number-of-jumps.py
@@ -3,12 +3,13 @@ input = sys.stdin.readline
 n = int(input())
 arr = list(map(int, input().split()))
 
-dp = [0]*n
-dp[0] = 1
+dp = [-1]*n
+dp[0] = 0
 for i in range(1,n):
-    maxval = 0
+    maxval = -1
     for j in range(i):
-        if i-j <= arr[j] and arr[j] > 0:
+        if i-j <= arr[j]:
             maxval = max(maxval, dp[j])
-    dp[i] = maxval+1
-print(max(dp)-1)
+    if maxval >=0:
+        dp[i] = maxval+1
+print(max(dp))
