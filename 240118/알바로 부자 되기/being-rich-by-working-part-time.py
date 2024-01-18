1df44a0a76f52e33ea2dee588caa9e33ed5a25a2
Dongwon Lee <ldw3097@gmail.com>
2024-01-18 09:02:09 +0900

    Title: 알바로 부자 되기, Time: 146ms, Memory: 25MB, Status: Passed - CodeTree

--- 240118/알바로 부자 되기/being-rich-by-working-part-time.py
+++ 240118/알바로 부자 되기/being-rich-by-working-part-time.py
@@ -4,7 +4,7 @@ n = int(input())
 arr = []
 for i in range(n):
     arr.append( list(map(int, input().split()))  )
-
+arr.sort(key=lambda x: x[1])
 enddaytomoney = {}
 
 for a,b,c in arr:
@@ -12,6 +12,9 @@ for a,b,c in arr:
     for endday in enddaytomoney:
         if endday < a:
             parmax = max(parmax, enddaytomoney[endday])
-    enddaytomoney[b] = parmax+c
+    if b not in enddaytomoney:
+        enddaytomoney[b] = parmax+c
+    else:
+        enddaytomoney[b] = max(enddaytomoney[b], parmax+c)
 
 print(max(enddaytomoney.values()))
