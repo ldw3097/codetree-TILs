6b417d85cd3396f41aad842cfcbedb2823e3b8fa
Dongwon Lee <ldw3097@gmail.com>
2024-01-18 13:19:03 +0900

    Title: 최대 동전 거슬러주기, Time: 205ms, Memory: 25MB, Status: Passed - CodeTree

--- 240118/최대 동전 거슬러주기/max-coin-change.py
+++ 240118/최대 동전 거슬러주기/max-coin-change.py
@@ -5,17 +5,13 @@ n, m = map(int, input().split())
 arr = []
 arr = list(map(int, input().split()))
 
-
-dq = deque()
-dq.append(0)
 dp = [-1]*(m+1)
 dp[0] = 0
-while dq:
-    a = dq.popleft()
-    b = dp[a]
+for i in range(1, m+1):
+    maxval = -1
     for coin in arr:
-        if a+coin <= m and dp[a+coin] < b+1:
-            dp[a+coin] = b+1
-            dq.append(a+coin)
-
+        if i-coin>=0 and dp[i-coin] != -1:
+            maxval = max(maxval, dp[i-coin])
+    if maxval != -1:
+        dp[i] = maxval+1
 print(dp[m])
