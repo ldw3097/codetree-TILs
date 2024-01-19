d0ce5633e2cc49b7f614910bd6f65808366062b3
Dongwon Lee <ldw3097@gmail.com>
2024-01-19 09:16:10 +0900

    Title: 최소 스패닝 트리, Time: 375ms, Memory: 28MB, Status: Wrong Answer - CodeTree

--- 240119/최소 스패닝 트리/minimum-spanning-tree.py
+++ 240119/최소 스패닝 트리/minimum-spanning-tree.py
@@ -18,9 +18,10 @@ def find(a):
     uf[a] = root
     return root 
 
-def union(x,y):
-    X, Y = find(x), find(y)
-    uf[X] = Y
+def union(a,b):
+    x = find(a)
+    y = find(b)
+    uf[a] = b
 
 edges.sort(key=lambda x : x[2])
 for i in range(1, n + 1):
