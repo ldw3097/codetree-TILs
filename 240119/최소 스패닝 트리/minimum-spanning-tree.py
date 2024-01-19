3678eef29e0178f8aed31c520ae1acb8a776ee8f
Dongwon Lee <ldw3097@gmail.com>
2024-01-19 09:12:44 +0900

    Title: 최소 스패닝 트리, Time: 255ms, Memory: 27MB, Status: Wrong Answer - CodeTree

--- 240119/최소 스패닝 트리/minimum-spanning-tree.py
+++ 240119/최소 스패닝 트리/minimum-spanning-tree.py
@@ -1,14 +1,15 @@
 import sys
 input = sys.stdin.readline
 sys.setrecursionlimit(10*7)
-n, m = map(int, input().split())
-arr = []
-for _ in range(m):
-    arr.append( list(map(int, input().split())) )
+n, m = tuple(map(int, input().split()))
+edges = []
+edges = [
+    tuple(map(int, input().split()))
+    for _ in range(m)
+]
 
 uf = [0]*(n + 1)
-for i in range(n + 1):
-    uf[i] = i
+
 
 def find(a):
     if uf[a] == a:
@@ -22,10 +23,12 @@ def union(a,b):
     y = find(b)
     uf[b] = a
     
-
-arr.sort(key=lambda x : x[2])
+for i in range(1, n + 1):
+    uf[i] = i
+    
+edges.sort(key=lambda x : x[2])
 ret = 0
-for a,b,c in arr:
+for a,b,c in edges:
     if find(a) != find(b):
         ret += c
         union(a,b)
