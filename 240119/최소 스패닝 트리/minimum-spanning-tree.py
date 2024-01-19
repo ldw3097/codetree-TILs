f64c195c30b4de08529319a0aea2df4fefce7895
Dongwon Lee <ldw3097@gmail.com>
2024-01-19 09:03:02 +0900

    Title: 최소 스패닝 트리, Time: 737ms, Memory: 38MB, Status: Passed - CodeTree

--- 240119/최소 스패닝 트리/minimum-spanning-tree.py
+++ 240119/최소 스패닝 트리/minimum-spanning-tree.py
@@ -1,33 +1,44 @@
-import sys
-input = sys.stdin.readline
-sys.setrecursionlimit(10*7)
-n, m = map(int, input().split())
-arr = []
-for _ in range(m):
-    arr.append( list(map(int, input().split())) )
+# 변수 선언 및 입력:
+n, m = tuple(map(int, input().split()))
+edges = [
+    tuple(map(int, input().split()))
+    for _ in range(m)
+]
 
-uf = [0]*10001
-for i in range(10001):
+uf = [0] * (n + 1)
+
+
+def find(x):
+    if uf[x] == x:
+        return x
+    uf[x] = find(uf[x])
+    return uf[x]
+
+
+def union(x, y):
+    X, Y = find(x), find(y)
+    uf[X] = Y
+
+
+# cost 순으로 오름차순 정렬을 진행합니다.
+edges.sort(key=lambda x: x[2])
+
+# uf 값을 초기값을 적어줍니다.
+for i in range(1, n + 1):
     uf[i] = i
 
-def find(a):
-    if uf[a] == a:
-        return a 
-    root = find(uf[a])
-    uf[a] = root
-    return root 
+# cost가 낮은 간선부터 순서대로 보며
+# 아직 두 노드가 연결이 되어있지 않을 경우에만
+# 해당 간선을 선택하고 두 노드를 합쳐주면서
+# mst를 만들어줍니다.
+ans = 0
+for x, y, cost in edges:
+    # x, y가 연결되어 있지 않다면
+    if find(x) != find(y):
+        # 해당 간선은 MST에 속하는 간선이므로
+        # 답을 갱신해주고
+        # 두 노드를 연결해줍니다.
+        ans += cost
+        union(x, y)
 
-def union(a,b):
-    x = find(a)
-    y = find(b)
-    if x == y:
-        return False
-    uf[b] = a
-    return True
-
-arr.sort(key=lambda x : x[2])
-ret = 0
-for a,b,c in arr:
-    if union(a,b):
-        ret += c
-print(ret)
+print(ans)
