43ea8d9fa348a0eb7f8114dcecd26447398e6181
Dongwon Lee <ldw3097@gmail.com>
2024-01-19 14:38:10 +0900

    Title: 최소 점프 횟수, Time: 143ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240119/최소 점프 횟수/min-num-of-jumps.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+from collections import *
+n = int(input())
+arr = list(map(int, input().split()))
+
+inf = 987654321
+dp = [inf]*n
+dp[0] = 0
+dq = deque([0])
+while dq:
+    a = dq.popleft()
+    val = arr[a]
+    for i in range(1, val+1):
+        if a+i < n and dp[a+i] > val + 1:
+            dp[a+i] = val+1
+            dq.append(a+i)
+            
+
+if dp[n-1] == inf:
+    print(-1)
+else:
+    print(dp[n-1])
