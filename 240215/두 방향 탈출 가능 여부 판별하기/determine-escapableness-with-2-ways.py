e2279a9c0d117aac7fe01bc05b1929c601f370e6
Dongwon Lee <ldw3097@gmail.com>
2024-02-15 08:13:03 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 159ms, Memory: 73MB, Status: Passed - CodeTree

--- 240215/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ 240215/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -3,15 +3,16 @@ arr = []
 for _ in range(n):
     arr.append(list(map(int, input().split())))
 
-def traverse(y, x):
+def traverse(y, x, visited):
     if (y,x) == (n-1, m-1):
         return 1
     for dy, dx in zip((1, 0), (0,1)):
         ny,nx = y+dy, x+dx
-        if 0<=ny <n and 0<=nx<m and arr[ny][nx] == 1:
-            if(traverse(ny,nx)):
+        if 0<=ny <n and 0<=nx<m and arr[ny][nx] == 1 and (ny,nx) not in visited:
+            visited.add((ny,nx))
+            if(traverse(ny,nx, visited)):
                 return 1
     return 0
 
-ret = traverse(0,0)
+ret = traverse(0,0, {(0,0),})
 print(ret)
